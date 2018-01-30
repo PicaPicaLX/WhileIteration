0b9041a2b3e92ca33f1e19a2b554d72ccb76e9a1
栗鑫 <lx389029980@outlook.com>
2018-01-30 22:42:59 +0800

    V1

--- WhileIteration/Program.cs
+++ WhileIteration/Program.cs
@@ -10,6 +10,46 @@ namespace WhileIteration
     {
         static void Main(string[] args)
         {
+
+            MainMenu();
+        }
+
+
+        private static void MainMenu()
+        {
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1) Option 1");
+            Console.WriteLine("2) Option 2");
+            Console.WriteLine("3) Exit");
+            string result = Console.ReadLine();
+            if (result=="1")
+            {
+                PrintNumbers();
+            }
+            else if (result == "2")
+            {
+                GuessingGame();
+            }
+            else if (result=="3")
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+
+        private static void PrintNumbers()
+        {
+            Console.WriteLine("Print numbers");
+            Console.ReadLine();
+        }
+
+        private static void GuessingGame()
+        {
+            Console.WriteLine("Guessing game!");
+            Console.ReadLine();
         }
     }
 }
