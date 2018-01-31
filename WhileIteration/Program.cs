5fbb7d959c1c65220684dcf8feefd35d4890ebd8
栗鑫 <lx389029980@outlook.com>
2018-01-31 20:10:41 +0800

    V1

--- WhileIteration/Program.cs
+++ WhileIteration/Program.cs
@@ -8,39 +8,46 @@ namespace WhileIteration
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // 创建一个控制台，能够玩游戏
         {
-
-            MainMenu();
+            bool displayMenu = true; // bool变量，类似布尔值
+            while (displayMenu == true) // while循环当括号内为true时，执行花括号里的，否则执行花括号外的
+            {
+                displayMenu=MainMenu(); // 调用自定义方法"MainMenu"
+            }
+            
         }
 
 
-        private static void MainMenu()
+        private static bool MainMenu() // 自定义方法"MainMenu"，在控制台显示以下选项并在其之后返回布尔值
         {
             Console.WriteLine("Choose an option:");
             Console.WriteLine("1) Option 1");
             Console.WriteLine("2) Option 2");
             Console.WriteLine("3) Exit");
-            string result = Console.ReadLine();
+
+            string result = Console.ReadLine(); // 读取用户输入
             if (result=="1")
             {
-                PrintNumbers();
+                PrintNumbers(); // 调用自定义方法"PrintNumbers"
+                return true;
             }
             else if (result == "2")
             {
-                GuessingGame();
+                GuessingGame(); // 调用自定义方法"GuessingGame"
+                return true;
             }
             else if (result=="3")
             {
-
+                return false; // 输入"3",返回false
             }
             else
             {
-
+                return true; // 输入其他值则返回true
             }
         }
 
-        private static void PrintNumbers()
+        private static void PrintNumbers() // 自定义方法"PrintNumbers"，在控制台显示"Print numbers"
         {
             Console.WriteLine("Print numbers");
             Console.ReadLine();
@@ -48,7 +55,7 @@ namespace WhileIteration
 
         private static void GuessingGame()
         {
-            Console.WriteLine("Guessing game!");
+            Console.WriteLine("Guessing game!"); // 自定义方法"Guessing game"，在控制台显示"Guessing game！"
             Console.ReadLine();
         }
     }
